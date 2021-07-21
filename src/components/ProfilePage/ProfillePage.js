85aafed96a3a55a04ff2d4de62ff60ab9bcbeaf4
mahela98 <chandimaofficial@gmail.com>
2021-07-21 09:28:16 +0530

    profile page done

--- src/components/ProfilePage/ProfillePage.js
+++ src/components/ProfilePage/ProfillePage.js
@@ -1,68 +1,50 @@
+import "./ProfilePage.css";
 const ProfilePage = () => {
+
     return (
-        <div className="w-full bg-gray-200 dark:bg-gray-900 py-10">
-            <div className="container mx-auto px-6 flex items-start justify-center">
-                <div className="w-full">
+        <>
+            <div className="rounded relative mt-0 h-48">
+                <img src="https://cdn.tuk.dev/assets/webapp/forms/form_layouts/form2.jpg" alt className="w-full h-full object-cover  absolute shadow" />
+                <div className="absolute  top-0 right-0 bottom-0 left-0  myImage" />
+                <div className="w-32 h-32 rounded-full bg-cover bg-center bg-no-repeat absolute bottom-0 -mb-10 ml-12 shadow flex items-center justify-center">
+                    <img src="https://cdn.tuk.dev/assets/webapp/forms/form_layouts/form2.jpg" alt className="absolute z-0 h-full w-full object-cover rounded-full shadow top-0 left-0 bottom-0 right-0" />
+                </div>
+            </div>
 
-                    <div className="flex flex-col lg:flex-row mx-auto bg-white dark:bg-gray-800 shadow rounded">
-                        <div className="w-full lg:w-3/3 px-12 flex flex-col items-center py-10">
-                            <div className="w-24 h-24 mb-3 p-1 rounded-full bg-gray-200 dark:bg-gray-700 flex items-center justify-center">
-                                <img className="w-full h-full overflow-hidden object-cover rounded-full" src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" alt="avatar" />
-                            </div>
-                            <h2 className="text-gray-800 dark:text-gray-100 text-xl tracking-normal font-medium mb-1">Timothy Jon Oliphant</h2>
-                            <p className="flex text-gray-600 dark:text-gray-100 text-sm tracking-normal font-normal mb-3 text-center">
-                                <span className="cursor-pointer mr-1 text-gray-600 dark:text-gray-100">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-pin" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" />
-                                        <circle cx={12} cy={11} r={3} />
-                                        <path d="M17.657 16.657L13.414 20.9a1.998 1.998 0 0 1 -2.827 0l-4.244-4.243a8 8 0 1 1 11.314 0z" />
-                                    </svg>
-                                </span>
-                                Las Vegas, Nevada
-                            </p>
-                            <p className="text-gray-600 dark:text-gray-100 text-sm tracking-normal font-normal mb-8 text-center w-10/12">The more I deal with the work as something that is my own, as something that is personal, the more successful it is.</p>
-                            <div className="flex items-start">
-                           
-                                <div className="mx-6 lg:mx-3 xl:mx-6 px-8 lg:px-4 xl:px-8 border-l border-r">
-                                    <h2 className="text-gray-600 dark:text-gray-100 text-2xl leading-6 mb-2 text-center">28</h2>
-                                    <p className="text-gray-800 dark:text-gray-100 text-sm leading-5">Projects</p>
+            <div className="w-full bg-gray-200 dark:bg-gray-900 py-0">
+                <div className="container mx-auto px-6 flex items-start justify-center">
+                    <div className="w-full">
+
+                        <div className="flex flex-col lg:flex-row mt-5 mb-5 mx-auto bg-white dark:bg-gray-800 shadow rounded">
+                            <div className="w-full lg:w-3/3 px-12 flex flex-col items-center py-10">
+
+                                <h2 className="text-gray-800 dark:text-gray-100 text-xl tracking-normal font-medium mb-1">Timothy Jon Oliphant</h2>
+                                <p className="flex text-gray-600 dark:text-gray-100 text-sm tracking-normal font-normal mb-3 text-center">
+                                    <span className="cursor-pointer mr-1 text-gray-600 dark:text-gray-100">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-pin" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" />
+                                            <circle cx={12} cy={11} r={3} />
+                                            <path d="M17.657 16.657L13.414 20.9a1.998 1.998 0 0 1 -2.827 0l-4.244-4.243a8 8 0 1 1 11.314 0z" />
+                                        </svg>
+                                    </span>
+                                    Las Vegas, Nevada
+                                </p>
+                                <p className="text-gray-600 dark:text-gray-100 text-sm tracking-normal font-normal mb-8 text-center w-10/12">The more I deal with the work as something that is my own, as something that is personal, the more successful it is.</p>
+                                <div className="flex items-start">
+
+                                    <div className="mx-6 lg:mx-3 xl:mx-6 px-8 lg:px-4 xl:px-8 border-l border-r">
+                                        <h2 className="text-gray-600 dark:text-gray-100 text-2xl leading-6 mb-2 text-center">28</h2>
+                                        <p className="text-gray-800 dark:text-gray-100 text-sm leading-5">Projects</p>
+                                    </div>
+
                                 </div>
-                                
                             </div>
-                        </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-
-
-    //     <div className="mx-auto container px-4 xl:px-0 py-10 xl:py-40">
-    //     <div className>
-    //         <div className="flex flex-wrap items-center">
-    //             <div className="lg:w-2/5 w-full ">
-    //                 <img className="w-full" src="https://cdn.tuk.dev/assets/templates/prodify/solution.png" alt srcSet />
-    //             </div>
-    //             <div className="lg:w-3/5 w-full">
-    //                 <div className="lg:pl-8 py-2 text-color">
-    //                     <h1 className="text-3xl xl:text-6xl leading-snug f-f-l font-black">Gather data to offer a bespoke solution</h1>
-    //                     <h2 className="text-xl lg:text-3xl leading-7 lg:leading-10 f-f-r py-4 lg:py-8">Here at Globex we take special care of what your organization needs instead of selling you a mass market tool that takes a one size fits all approach. I personally review each and every client business and oversee the team that tailores a solution</h2>
-    //                     <div className="flex items-center">
-    //                         <div className="flex items-center justify-center rounded-full w-16 h-16">
-    //                             <img src="https://cdn.tuk.dev/assets/templates/prodify/profile.png" alt />
-    //                         </div>
-    //                         <div className="pl-3">
-    //                             <h3 className="f-f-l text-xl lg:text-3xl">Rachel Dawson</h3>
-    //                             <h4 className="f-f-r text-lg pt-2">CEO, Globex Workspaces</h4>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>
-
+        </>
     );
 }
 
--- src/components/test1.js
+++ src/components/test1.js
@@ -1,9 +1,41 @@
 const Test = () => {
     return (
 
-        <div className="grid grid-cols-1 gap-10 px-5 pb-20  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="grid grid-cols-1 gap-10 px-5 pb-20 pt-10  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg ">
                 <img className="w-full" src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="Sunset in the mountains" />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                    <p className="text-gray-700 text-base">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div className="px-6 pt-4 pb-2 ">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+
+
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img className="w-full" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="Sunset in the mountains" />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                    <p className="text-gray-700 text-base">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div className="px-6 pt-4 pb-2">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+
+
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img className="w-full" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="Sunset in the mountains" />
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
                     <p className="text-gray-700 text-base">
@@ -33,6 +65,12 @@ const Test = () => {
                 </div>
             </div>
         </div>
+
+
+
+
+
+
 
     );
 }
