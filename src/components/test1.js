2c4a5826f08deb804bb8d7229853896e5cc8eff6
mahela98 <chandimaofficial@gmail.com>
2021-07-31 00:05:29 +0530

    Modal half done

--- src/components/test1.js
+++ src/components/test1.js
@@ -2,16 +2,29 @@
 import { Fragment, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { ExclamationIcon } from '@heroicons/react/outline'
-
-
-
+import { Redirect } from 'react-router-dom';
 
 export default function Example(props) {
   console.log(props.book)
-  const [open, setOpen] = useState(false)
-
-  const cancelButtonRef = useRef(null)
-
+  const [open, setOpen] = useState(false);
+  const cancelButtonRef = useRef(null);
+  const {
+    author,
+    book_image,
+    buy_links,
+    description,
+    price,
+    primary_isbn10,
+    publisher,
+    rank_last_week,
+    rank,
+    title,
+    saleInfo,
+    volumeInfo,
+    id
+  } = props.book;
+try {
+  
   return (
     <div>
       <button onClick={()=>setOpen(true)}>kjahsd</button>
@@ -24,7 +37,7 @@ export default function Example(props) {
         open={open}
         onClose={setOpen}
       >
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="flex items-end  justify-center min-h-screen pt-0 px-o pb-20 text-center sm:block sm:p-0">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -34,7 +47,7 @@ export default function Example(props) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <Dialog.Overlay className="fixed  inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
           {/* This element is to trick the browser into centering the modal contents. */}
@@ -50,36 +63,106 @@ export default function Example(props) {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="inline-block pt-30 align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all ">
+              <div className="bg-white px-0 pt-0 pb-4 sm:p-0 sm:pb-0">
                 <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                  {/* <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                     <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-                  </div>
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                      {props.title}
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Are you sure you want to deactivate your account? All of your data will be permanently removed.
-                        This action cannot be undone.
-                      </p>
+                  </div> */}
+                  <div className="mt-0 text-center sm:mt-0 sm:ml-0 pt-20 sm:text-left">
+                    {/* <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                      {title}
+                    </Dialog.Title> */}
+                    <div className="mt-0">
+                    
+
+
+
+
+<>
+        <section className="book-details-body body-font overflow-hidden">
+          <div className="container px-5 py-10 lg:py-14 mx-auto ">
+            <div className=" sm:w-4/5 mx-auto flex flex-wrap content-center my-boder">
+              <img alt="ecommerce" className="w-4/5 pl-20 sm:pl-0 sm:w-1/2 xl:w-1/3  lg:h-auto h-full object-cover  object-center rounded" src={book_image || `https://books.google.com/books?id=${id}&printsec=frontcover&img=1&zoom=4&source=gbs_api`} />
+              <div className="lg:w-1/2 w-full lg:pl-10  lg:py-6 mt-6 lg:mt-0">
+                <h1 className="my-title-text text-3xl title-font font-medium mb-1">{title || volumeInfo.title}</h1>
+                <h2 className="text-sm title-font text-white-500 tracking-widest text-lg">Author- {author || volumeInfo.authors || "Anonymous"}</h2>
+
+                <div className="flex mb-4">
+                  <span className="flex items-center">
+                    <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <span className="text-gray-200 ml-3">Rank <strong>{rank || "None"}</strong> </span>
+                  </span>
+                  <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
+                    <div>Last Week Rank <strong>{rank_last_week || "None"}</strong> </div>
+                  </span>
+                </div>
+
+                <div className="flex py-3 px-5 sm:px-0">
+                  {
+                    price && (<span className="title-font font-medium text-xl text-gray-200 my-title-text">Price : $ {price}</span>)
+                  }
+                  {!price &&
+                    saleInfo.retailPrice && (<span className="title-font font-medium text-xl text-gray-200 my-title-text">Price : {saleInfo.retailPrice.amount && saleInfo.retailPrice.amount} {saleInfo.retailPrice.currencyCode && saleInfo.retailPrice.currencyCode} </span>
+                    )
+                  }
+                </div>
+
+                <p className="leading-relaxed">{description || volumeInfo.description}</p>
+                <h2 className="text-sm title-font text-gray-300 tracking-widest pt-5 ">Published By {publisher || volumeInfo.publisher} Books.</h2>
+
+
+{/* 
+                <div className="mt-5">
+                  <h4 className="mb-2 text-lgtitle-font font-medium mb-1">Buy Links</h4>
+                  <ByLinksComponent buy_links = {buy_links || null} saleInfo = {saleInfo || null} />
+                
+                </div> */}
+
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+
+
+
+
+
+
+
+      </>
+
+
+
+
+
+
+
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <div className="bg-gray-50 px-4 py-0 sm:px-0 sm:flex sm:flex-row-reverse">
+         
                 <button
                   type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => setOpen(false)}
-                >
-                  Deactivate
-                </button>
-                <button
-                  type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="mt-3 mb-6 mx-6 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={() => setOpen(false)}
                   ref={cancelButtonRef}
                 >
@@ -93,4 +176,11 @@ export default function Example(props) {
     </Transition.Root>
     </div>
   )
+} catch (error) {
+  { console.log(error) }
+  return (
+    <Redirect to='/' />
+  );
+}
+
 }
